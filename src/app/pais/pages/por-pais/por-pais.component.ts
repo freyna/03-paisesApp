1be8e1c389b7c8f994254f5262a78725786d5a4b
Filiberto Reyna <ing.filiberto.reyna@gmail.com>
2022-02-14 08:26:31 -0600

    Creación de servicio y consumo de api búsqueda por pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar() {
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino).subscribe(resp=>{
+      
+    });
   }
 
 }
