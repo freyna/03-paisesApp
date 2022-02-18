47496e1205f1eee85613df9cc5563ddb0e95ef7a
Filiberto Reyna <ing.filiberto.reyna@gmail.com>
2022-02-18 07:55:14 -0600

    Optimización de peticiones http en el servicio

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Pais } from '../interfaces/pais.interface';
@@ -10,24 +10,26 @@ export class PaisService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
+  private httpParams = new HttpParams().set("fields","name,capital,flags,population,ccn3")
+
   constructor(private http: HttpClient) { }
 
   buscarPais(termino: string): Observable<Pais[]>{ 
 
     const url = `${this.apiUrl}/name/${termino}`
-    return this.http.get<Pais[]>(url);
+    return this.http.get<Pais[]>(url, { params:this.httpParams });
   }
 
   buscarPorCapital (termino: string): Observable<Pais[]>{ 
 
     const url = `${this.apiUrl}/capital/${termino}`
-    return this.http.get<Pais[]>(url);
+    return this.http.get<Pais[]>(url, { params: this.httpParams });
   }
 
   buscarPorRegion (termino: string): Observable<Pais[]>{ 
 
     const url = `${this.apiUrl}/region/${termino}`
-    return this.http.get<Pais[]>(url);
+    return this.http.get<Pais[]>(url, { params: this.httpParams });
   }
 
   obtenerPorCodigo (termino: string): Observable<Pais[]>{ 
