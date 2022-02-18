18aabeefb936dde8c522406e5b7fbcd38ba9540a
Filiberto Reyna <ing.filiberto.reyna@gmail.com>
2022-02-18 07:39:11 -0600

    Modificaciones a página por región

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -6,17 +6,51 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `
+      button{
+        margin-right: 5px;
+      }
+    `
   ]
 })
 export class PorRegionComponent implements OnInit {
   termino: string = '';
   hayError: boolean = false;
   paises: Pais[] = [];
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activeRegion: string = '';
 
 
   constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  obtenerClase(region: string): string {
+    return region === this.activeRegion 
+          ? 'btn btn-primary'
+          : 'btn btn-outline-primary'
+  }
+
+  obtenerPaisesRegion(region: string) {
+    console.log("obtenerPaisesRegion", region);
+    this.activeRegion = region;
+    this.hayError = false;
+    console.log(this.termino);
+
+    this.paisService.buscarPorRegion(this.activeRegion)
+    .subscribe( paises => {
+      if(paises.length < 1) {
+        this.hayError = true;
+      }
+      console.info(paises);
+      this.paises = paises;      
+      
+    }, (err)=>{
+      console.log("Error");
+      console.info(err);
+      this.hayError = true;
+    });
   }
 
   buscar(termino: string) {
